f5ea19731627d8ecbebcc12c61d27af06837eb35
Mbalenhle <mbalindaba01@gmail.com>
2021-09-09 17:12:37 +0200

    removed factory function tests

--- test/languagePicker.test.js
+++ test/languagePicker.test.js
@@ -1,57 +1,57 @@
-let assert = require('assert')
-let LanguagePicker = require('../languagePicker')
+// let assert = require('assert')
+// let LanguagePicker = require('../languagePicker')
 
-describe('LanguagePicker function', () => {
-    describe('Set Values', () => {
-        it('should be able to set the language of the greeting to IsiXhosa', () => {
-            let pickLanguage = LanguagePicker()
-            pickLanguage.setLanguage('IsiXhosa')
+// describe('LanguagePicker function', () => {
+//     describe('Set Values', () => {
+//         it('should be able to set the language of the greeting to IsiXhosa', () => {
+//             let pickLanguage = LanguagePicker()
+//             pickLanguage.setLanguage('IsiXhosa')
 
-            assert.strictEqual('IsiXhosa', pickLanguage.getLanguage())
-        });
+//             assert.strictEqual('IsiXhosa', pickLanguage.getLanguage())
+//         });
 
-        it('should be able to set the language of the greeting to IsiZulu', () => {
-            let pickLanguage = LanguagePicker()
-            pickLanguage.setLanguage('IsiZulu')
+//         it('should be able to set the language of the greeting to IsiZulu', () => {
+//             let pickLanguage = LanguagePicker()
+//             pickLanguage.setLanguage('IsiZulu')
 
-            assert.strictEqual('IsiZulu', pickLanguage.getLanguage())
-        });
+//             assert.strictEqual('IsiZulu', pickLanguage.getLanguage())
+//         });
 
-        it('should be able to set the language of the greeting to English', () => {
-            let pickLanguage = LanguagePicker()
-            pickLanguage.setLanguage('English')
+//         it('should be able to set the language of the greeting to English', () => {
+//             let pickLanguage = LanguagePicker()
+//             pickLanguage.setLanguage('English')
 
-            assert.strictEqual('English', pickLanguage.getLanguage())
-        });
+//             assert.strictEqual('English', pickLanguage.getLanguage())
+//         });
         
-    });
+//     });
 
-    describe('Use Values', () => {
-        it('should return "Sawubona " if the language of greeting is IsiZulu', () => {
-            let pickLanguage = LanguagePicker()
-            pickLanguage.setLanguage('IsiZulu')
-            pickLanguage.setGreetText('Sawubona ')
+//     describe('Use Values', () => {
+//         it('should return "Sawubona " if the language of greeting is IsiZulu', () => {
+//             let pickLanguage = LanguagePicker()
+//             pickLanguage.setLanguage('IsiZulu')
+//             pickLanguage.setGreetText('Sawubona ')
 
-            assert.strictEqual('Sawubona ', pickLanguage.getGreetText())
-        });
+//             assert.strictEqual('Sawubona ', pickLanguage.getGreetText())
+//         });
 
-        it('should return "Molo " if the language of greeting is IsiXhosa', () => {
-            let pickLanguage = LanguagePicker()
-            pickLanguage.setLanguage('IsiXhosa')
-            pickLanguage.setGreetText('Molo ')
+//         it('should return "Molo " if the language of greeting is IsiXhosa', () => {
+//             let pickLanguage = LanguagePicker()
+//             pickLanguage.setLanguage('IsiXhosa')
+//             pickLanguage.setGreetText('Molo ')
 
-            assert.strictEqual('Molo ', pickLanguage.getGreetText())
-        });
+//             assert.strictEqual('Molo ', pickLanguage.getGreetText())
+//         });
 
-        it('should return "Hello " if the language of greeting is English', () => {
-            let pickLanguage = LanguagePicker()
-            pickLanguage.setLanguage('English')
-            pickLanguage.setGreetText('Hello ')
+//         it('should return "Hello " if the language of greeting is English', () => {
+//             let pickLanguage = LanguagePicker()
+//             pickLanguage.setLanguage('English')
+//             pickLanguage.setGreetText('Hello ')
 
-            assert.strictEqual('Hello ', pickLanguage.getGreetText())
-        })
+//             assert.strictEqual('Hello ', pickLanguage.getGreetText())
+//         })
         
-    });
+//     });
     
     
-});
+// });
--- test/namesGreeted.test.js
+++ test/namesGreeted.test.js
@@ -1,29 +1,29 @@
-let assert = require('assert')
-let NamesGreeted = require('../namesGreeted')
+// let assert = require('assert')
+// let NamesGreeted = require('../namesGreeted')
 
-describe('NamesGreeted Function', () => {
-    describe('Set Values', () => {
-        it('should be able to set the name to zuki', () => {
-            let greetedNames = NamesGreeted()
-            greetedNames.setName('Zuki')
+// describe('NamesGreeted Function', () => {
+//     describe('Set Values', () => {
+//         it('should be able to set the name to zuki', () => {
+//             let greetedNames = NamesGreeted()
+//             greetedNames.setName('Zuki')
 
-            assert.strictEqual('zuki', greetedNames.getName())
-        });
+//             assert.strictEqual('zuki', greetedNames.getName())
+//         });
 
-        it('should transform Mbali to mbali', () => {
-            let greetedNames = NamesGreeted()
-            greetedNames.setName('Mbali')
+//         it('should transform Mbali to mbali', () => {
+//             let greetedNames = NamesGreeted()
+//             greetedNames.setName('Mbali')
 
-            assert.strictEqual('mbali', greetedNames.getName())
-        });
+//             assert.strictEqual('mbali', greetedNames.getName())
+//         });
 
-        it('should transform SIYABONGA to siyabonga', () => {
-            let greetedNames = NamesGreeted()
-            greetedNames.setName('SIYABONGA')
+//         it('should transform SIYABONGA to siyabonga', () => {
+//             let greetedNames = NamesGreeted()
+//             greetedNames.setName('SIYABONGA')
 
-            assert.strictEqual('siyabonga', greetedNames.getName())
-        });
+//             assert.strictEqual('siyabonga', greetedNames.getName())
+//         });
         
-    });
+//     });
     
-});
+// });
